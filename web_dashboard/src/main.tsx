e8bd4c8b5d15591641840e0e108fc8182f69bb11
Mr.Quantum <darshanpatelgdh@gmail.com>
2025-08-30 17:32:16 +0530

    ..

--- web_dashboard/src/main.tsx
+++ web_dashboard/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './contexts/AuthContext'
-import { Toaster } from './components/ui/toaster'
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import { Toaster } from './components/ui/toaster.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
